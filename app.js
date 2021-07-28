b95b9ded16742752fcac836e546a2ae8996dc280
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-07-28 20:06:11 +0100

    use express and socket on the same port

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-const express = require("express")
-require('dotenv').config();
-const app = express()
+require('dotenv').config()
+var cors = require('cors')
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: [
+            "https://toihirhalim.github.io/tictactoe-react-redux",
+            process.env.FRONTEND_URL
+        ]
+    }
+});
+
+app.use(cors({
+    origin: ['https://toihirhalim.github.io/tictactoe-react-redux', process.env.FRONTEND_URL]
+}))
 
 app.get("/", function (req, res) {
     res.redirect(process.env.FRONTEND_URL || "https://toihirhalim.github.io/tictactoe-react-redux")
 })
 
-app.listen(process.env.PORT || 3000,
-    () => console.log("Server is running..."));
+let online = 0
+
+io.on('connection', socket => {
+    console.log("connected " + socket.id)
+    console.log(++online + " online")
+
+    socket.on('disconnect', function () {
+        console.log(socket.id + ' disconnected')
+        console.log(--online + " online")
+    });
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('server is running on port ' + server.address().port)
+});
 
