e0a188a8434d8a00e83355f694cd50af6c295117
ToihirHalim <66559721+ToihirHalim@users.noreply.github.com>
2021-08-15 13:40:18 +0100

    update register validator: limit password at 16 chars

--- auth/validators.js
+++ auth/validators.js
@@ -11,16 +11,23 @@ module.exports = {
                 .withMessage('username is required')
                 .not()
                 .custom((val) => /[^A-za-z0-9]/g.test(val))
-                .withMessage('Username can not use symbols'),
+                .withMessage('Username can not use symbols')
+                .isLength({ min: 3 })
+                .withMessage('Username must be at least 3 characters')
+                .isLength({ max: 20 })
+                .withMessage('Username must not be over 20 characters'),
             oneOf([
                 check('email').isEmail().withMessage('Please enter a valid email'),
-                check('email').isEmpty().withMessage('Please enter a valid email again')
+                check('email').isEmpty().withMessage('Please enter a valid email')
             ]),
             check('password')
                 .notEmpty()
                 .withMessage('password is required')
                 .isLength({ min: 8 })
-                .withMessage('password must be 8 characters')
+                .withMessage('password must be at least 8 characters')
+                .withMessage('password is required')
+                .isLength({ max: 16 })
+                .withMessage('password must not be over 16 characters')
         ]
     },
     loginValidator: () => {
